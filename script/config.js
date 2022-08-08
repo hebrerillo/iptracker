c5eac050b9f22d8ab59822432832d309907a4479
hebrerillo <hebrerillo@gmail.com>
2022-08-08 11:46:15 +0200

    Changing some permissions

--- script/config.js
+++ script/config.js
@@ -1,1 +1,2 @@
-export const API_KEY = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_KEY = 'at_rHL5xVdKVTJAt6uiNipQ6KJBPBOin';
+export const GEOIPFY_URI = 'https://geo.ipify.org/api/v2/country,city';
--- script/ipTracker.js
+++ script/ipTracker.js
@@ -2,11 +2,20 @@ import {API_KEY} from './config.js';
 
 function IpTracker()
 {
-    this.map = null;
+    this.map = null; //The leaflet map.
     this.init();
+    
 }
 
 IpTracker.prototype.init = function ()
+{
+    this.initMap();
+};
+
+/**
+ * Initialises the Leaflet map;
+ */
+IpTracker.prototype.initMap = function ()
 {
     this.map = L.map('map').setView([39.4640585,-3.5306775], 13);
 
@@ -14,5 +23,6 @@ IpTracker.prototype.init = function ()
         attribution: '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 };
+
 
 let appTracker = new IpTracker();
