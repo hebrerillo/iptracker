e32095d69e786945a7a45358dc99c83f38bea50b
hebrerillo <hebrerillo@gmail.com>
2022-08-11 18:59:40 +0200

    Using the proper URI

--- script/config.js
+++ script/config.js
@@ -1,3 +1,3 @@
 export const API_KEY = 'at_rHL5xVdKVTJAt6uiNipQ6KJBPBOin';
-export const GEOIPFY_URI = 'http://192.168.1.44/tests/mentor/ip-address-tracker-master/misc/api/index.php';
+export const GEOIPFY_URI = 'https://geo.ipify.org/api/v2/country,city';
 export const MAP_ZOOM = 13;
